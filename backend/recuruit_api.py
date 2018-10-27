a235dc73db56237e533d824480e5e9050a784ed5
s_naoya <sugita6878@gmail.com>
2018-10-27 17:13:40 +0900

    rename get_shop_name

--- backend/recuruit_api.py
+++ backend/recuruit_api.py
@@ -2,7 +2,7 @@ import requests
 import os
 import json
 
-def get_shop_name(lat: float, lng: float, count: int = 10) -> str:
+def get_shop_info(lat: float, lng: float, count: int = 10) -> str:
     api_key = os.environ['RECRUIT_API']
 
     url = "http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key={}&lat={}&lng={}&range=5&order=4&count={}&format=json".format(api_key, lat, lng, count)
@@ -14,7 +14,8 @@ def get_shop_name(lat: float, lng: float, count: int = 10) -> str:
 def create_data(responses: dict) -> dict:
     datas = {'shops':[]}
     for res in responses['results']['shop']:
-        data = {'name':res['name'],
+        data = {
+                'name':res['name'],
                 'address':res['address'],
                 'location':{
                     'lat':res['lat'],
