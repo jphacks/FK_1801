69649c3ee14bfa3dd5bb07e2e3067de7ee29818a
s_naoya <sugita6878@gmail.com>
2018-10-27 22:21:35 +0900

    add id

--- backend/recuruit_api.py
+++ backend/recuruit_api.py
@@ -3,7 +3,8 @@ import os
 import json
 
 def get_shop_info(lat: float, lng: float, count: int = 10) -> str:
-    api_key = os.environ['RECRUIT_API']
+    api_key = os.environ('RECRUIT_API')
+
 
     url = "http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key={}&lat={}&lng={}&range=5&order=4&count={}&format=json".format(api_key, lat, lng, count)
 
@@ -15,6 +16,7 @@ def create_data(responses: dict) -> dict:
     datas = {'shops':[]}
     for res in responses['results']['shop']:
         data = {
+                'id':res['id'],
                 'name':res['name'],
                 'address':res['address'],
                 'location':{
@@ -26,3 +28,7 @@ def create_data(responses: dict) -> dict:
                 }
         datas['shops'].append(data)
     return datas
+
+a = get_shop_info(33.5903689,130.4217697, 3)
+b = create_data(a)
+print(b)
