1315971a8ad547ab4d012a655a297f3247c6281e
s_naoya <sugita6878@gmail.com>
2018-10-27 15:38:29 +0900

    返り値をjsonにする

--- backend/recuruit_api.py
+++ backend/recuruit_api.py
@@ -1,10 +1,11 @@
 import requests
 import os
+import json
 
 def get_shop_name(lat: float, lng: float) -> str:
     api_key = os.environ['RECRUIT_API']
 
     url = "http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key={}&lat={}&lng={}&range=5&order=4&format=json".format(api_key, lat, lng)
 
-    resp = requests.get(url).text
+    resp = requests.get(url).json()
     return resp
